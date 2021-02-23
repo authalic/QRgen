d56aaadb913ab09837de4ccd7f8616ecef73d11a
Justin Johnson <authalic@gmail.com>
2021-02-23 13:29:48 -0700

    works as needed

--- qrgen.py
+++ qrgen.py
@@ -5,9 +5,10 @@
 # second command line parameter
 
 import argparse
-from os.path import join
+from os import makedirs
+from os.path import join, exists
 from urllib.parse import urlparse
-import qrcode  # https://pypi.org/project/qrcode/
+import qrcode  # see: https://pypi.org/project/qrcode/  for options
 
 
 # configure the command-line parsing
@@ -19,26 +20,44 @@ parser.add_argument("-o", "--outdir", help="name of output folder for QR image f
 args = parser.parse_args()
 
 
-
 # parse the input text file name and output directory from the command line parameters
+
+# check if the optional output directory was included
+outdir = args.outdir
+
+# create the path in os if it does not already exist
+if outdir:
+    if not exists(outdir):
+        makedirs(outdir)
 
 # input filename
 infile = args.urlfile
 
-# output directory (defaults to current dir)
-if args.outdir:
-    outdir = args.outdir
-else:
-    outdir = ''
-
-# (join(outdir, "testfile.txt"))
-
-
 # open the input file
 with open(infile) as f:
     urllist = f.readlines()
+
+    # loop through the URLs from the input file
     for url in urllist:
 
+        # create the output filename string
+        # replace slashes in URL with underscores and dots with dashes
+        # TODO: make this less klunky
+
+        o = urlparse(url) # remove the newline character from end of line
+        netloc = o.netloc.replace('.', '-')
+
+        if len(o.path.strip()) > 1: # an empty path is the '/' character
+            path = o.path.replace(r'/', '_').replace('.', '-')
+            outfilename = netloc + '_' + path.strip().strip('_') + '.png'
+        else:
+            outfilename = netloc + '.png'
+
+        # prepend the optional output folder to the output file name
+        if outdir:
+            outfilename = join(outdir, outfilename)
+
+        # generate the QR code
         qr = qrcode.QRCode(
             version=None,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -49,7 +68,5 @@ with open(infile) as f:
         qr.make(fit=True)
 
         img = qr.make_image(fill_color="black", back_color="white")
-
-        outfilename = ''
 
         img.save(outfilename, "PNG")
